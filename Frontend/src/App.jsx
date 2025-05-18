ea6aabc7c72c846ef3d240f5e484e163dd216b6a
KhushiChauhan8 <kc6478576@gmail.com>
2025-05-18 20:46:28 +0530

    Updated backend URL

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     const sound = new Audio('/click.mp3');
     sound.play();
     
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://code-reviewer-backend-h0mqfvdfx-khushi-chauhans-projects.vercel.app/ai/get-review', { code })
     setReview(response.data)
   }
 
